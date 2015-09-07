5bef1eee2a15d842c722fb48d71b93505782c825
Take Vos <take.vos@vosgames.nl>
2015-09-07 23:12:00 +0200

    Initial commit

---
+++ DimensionsCAM/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DimensionsCAM
+//
+//  Created by Take Vos on 2015-09-07.
+//  Copyright © 2015 VOSGAMES. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
